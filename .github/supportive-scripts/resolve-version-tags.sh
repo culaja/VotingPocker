af4bfd9b50610793b79404ab76a093e39180a438
Stanko Culaja <stanko.culaja@kolotree.com>
2021-07-05 08:06:47 +0200

    Try 1: changed way of versioning containers.

--- .github/supportive-scripts/resolve-version-tags.sh
+++ .github/supportive-scripts/resolve-version-tags.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 IMAGES_ENV_FILE_PATH=$1
-SHORT_SHA=$2
+COMMIT_SHA=$2
 
-sed -i "s/=latest/=${SHORT_SHA}/" "$IMAGES_ENV_FILE_PATH"
+SHORT_COMMIT_SHA=$(echo "${COMMIT_SHA}" | cut -c1-8)
+IMAGES_FOR_RELEASE=$(git tag --points-at "${COMMIT_SHA}" | grep "release=" | sed "s/release=//")
+echo "***"
+echo "${IMAGES_FOR_RELEASE}"
+echo "***"
+
+sed -i "s/=latest/=${SHORT_COMMIT_SHA}/" "$IMAGES_ENV_FILE_PATH"
